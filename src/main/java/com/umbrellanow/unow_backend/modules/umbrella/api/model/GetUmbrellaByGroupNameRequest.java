2a23f9ce0f708cd88a0dbdc49a271d6fb1fe8947
Injector1 <injector005@gmail.com>
2025-01-01 23:53:14 +0100

    Refactoring - added constructor decorators to Request/Response models

--- src/main/java/com/umbrellanow/unow_backend/modules/umbrella/api/model/GetUmbrellaByGroupNameRequest.java
+++ src/main/java/com/umbrellanow/unow_backend/modules/umbrella/api/model/GetUmbrellaByGroupNameRequest.java
@@ -1,8 +1,12 @@
 package com.umbrellanow.unow_backend.modules.umbrella.api.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetUmbrellaByGroupNameRequest {
     String groupName;
 }
--- src/main/java/com/umbrellanow/unow_backend/modules/umbrella/api/model/UmbrellaPriceRateResponse.java
+++ src/main/java/com/umbrellanow/unow_backend/modules/umbrella/api/model/UmbrellaPriceRateResponse.java
@@ -1,10 +1,14 @@
 package com.umbrellanow.unow_backend.modules.umbrella.api.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UmbrellaPriceRateResponse {
     private double hourlyRate;
     private double dailyRate;
