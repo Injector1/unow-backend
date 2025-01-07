57b869c82719fd273b24b56be438abb73d5abffe
Injector1 <injector005@gmail.com>
2025-01-07 22:45:26 +0100

    Integrate Email Provider - Integrated Gmail for one-time code sending, added necessary parameters to application-example.properties

--- src/main/java/com/umbrellanow/unow_backend/integrations/mail/EmailService.java
+++ src/main/java/com/umbrellanow/unow_backend/integrations/mail/EmailService.java
@@ -1,5 +1,6 @@
 package com.umbrellanow.unow_backend.integrations.mail;
 
 public interface EmailService {
+    void sendEmail(String to, String subject, String text);
     void sendOneTimeCode(String email, String code);
 }
--- src/main/java/com/umbrellanow/unow_backend/integrations/mail/EmailServiceImpl.java
+++ src/main/java/com/umbrellanow/unow_backend/integrations/mail/EmailServiceImpl.java
@@ -2,10 +2,34 @@ package com.umbrellanow.unow_backend.integrations.mail;
 
 import org.springframework.stereotype.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
 @Service
 public class EmailServiceImpl implements EmailService {
+    private final JavaMailSender mailSender;
+
+    @Autowired
+    public EmailServiceImpl(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
+    }
+
     @Override
     public void sendOneTimeCode(String email, String code) {
-        System.out.println(code);
+        sendEmail(
+                email,
+                "Your one time code for UmbrellaNow application",
+                "Hi! Here is your one-time code for login " + code
+        );
     }
 }
