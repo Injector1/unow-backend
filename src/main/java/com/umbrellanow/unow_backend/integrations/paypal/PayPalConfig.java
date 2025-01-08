74e9efe1b3c69f0d49e3c977c2362b527c6a5c82
Injector1 <injector005@gmail.com>
2025-01-08 22:56:21 +0100

    PayPal integration - PayPal config fix

--- src/main/java/com/umbrellanow/unow_backend/integrations/paypal/PayPalConfig.java
+++ src/main/java/com/umbrellanow/unow_backend/integrations/paypal/PayPalConfig.java
@@ -2,6 +2,7 @@ package com.umbrellanow.unow_backend.integrations.paypal;
 
 import com.paypal.core.PayPalEnvironment;
 import com.paypal.core.PayPalHttpClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +10,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties(prefix = "paypal")
 public class PayPalConfig {
+    @Value("${paypal.client.id}")
     private String clientId;
+    @Value("${paypal.client.secret}")
     private String clientSecret;
+    @Value("${paypal.mode}")
     private String mode;
 
     @Bean
