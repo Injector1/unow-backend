823552686d941d1a10856fdfc633956e6ee5277f
Injector1 <injector005@gmail.com>
2025-01-14 23:11:40 +0100

    Fix - added rental type parsing

--- src/main/java/com/umbrellanow/unow_backend/shared/enumeration/RentalType.java
+++ src/main/java/com/umbrellanow/unow_backend/shared/enumeration/RentalType.java
@@ -1,6 +1,21 @@
 package com.umbrellanow.unow_backend.shared.enumeration;
 
 public enum RentalType {
-    DAILY,
-    HOURLY;
+    DAILY("daily"),
+    HOURLY("hourly");
+
+    private final String typeName;
+
+    RentalType(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public static RentalType parseType(String type) {
+        for (RentalType value : values()) {
+            if (value.typeName.equals(type.strip().toLowerCase())) {
+                return value;
+            }
+        }
+        return null;
+    }
 }
