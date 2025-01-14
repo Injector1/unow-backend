e6da76cd80d88f740df242ed7be4b80f3a815bed
Injector1 <injector005@gmail.com>
2025-01-14 23:12:43 +0100

    Umbrella return - implemented searching for rental by umbrella id, user id and status

--- src/main/java/com/umbrellanow/unow_backend/modules/rental/infrastructure/RentalRepository.java
+++ src/main/java/com/umbrellanow/unow_backend/modules/rental/infrastructure/RentalRepository.java
@@ -1,6 +1,7 @@
 package com.umbrellanow.unow_backend.modules.rental.infrastructure;
 
 import com.umbrellanow.unow_backend.modules.rental.infrastructure.entity.Rental;
+import com.umbrellanow.unow_backend.shared.enumeration.RentalStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,4 +9,5 @@ import java.util.List;
 public interface RentalRepository extends JpaRepository<Rental, Long> {
     List<Rental> findAllByUser_id(long userID);
     List<Rental> findAllByUmbrella_id(long umbrellaID);
+    List<Rental> findAllByUmbrella_idAndUser_idAndStatus(Long umbrellaID, Long userID, RentalStatus status);
 }
