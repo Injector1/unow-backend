05b1bcadc2b3d8bebb8031e6d132a3ce7d73013d
Injector1 <injector005@gmail.com>
2025-01-19 17:10:59 +0100

    Rental - added logic for handling storage box properties

--- src/main/java/com/umbrellanow/unow_backend/modules/storage/domain/StorageService.java
+++ src/main/java/com/umbrellanow/unow_backend/modules/storage/domain/StorageService.java
@@ -1,7 +1,10 @@
 package com.umbrellanow.unow_backend.modules.storage.domain;
 
 import com.umbrellanow.unow_backend.modules.storage.infrastructure.entity.StorageBox;
+import com.umbrellanow.unow_backend.modules.umbrella.infrastructure.entity.Umbrella;
 
 public interface StorageService {
     StorageBox findEmptyStorageBox();
+    StorageBox markStorageAsEmpty(StorageBox box);
+    StorageBox setAssociatedUmbrellaForBox(StorageBox box, Umbrella umbrella);
 }
--- src/main/java/com/umbrellanow/unow_backend/modules/storage/domain/StorageServiceImpl.java
+++ src/main/java/com/umbrellanow/unow_backend/modules/storage/domain/StorageServiceImpl.java
@@ -2,6 +2,7 @@ package com.umbrellanow.unow_backend.modules.storage.domain;
 
 import com.umbrellanow.unow_backend.modules.storage.infrastructure.StorageBoxRepository;
 import com.umbrellanow.unow_backend.modules.storage.infrastructure.entity.StorageBox;
+import com.umbrellanow.unow_backend.modules.umbrella.infrastructure.entity.Umbrella;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,5 +19,18 @@ public class StorageServiceImpl implements StorageService {
     @Override
     public StorageBox findEmptyStorageBox() {
         return storageBoxRepository.findAllEmpty().stream().findFirst().orElse(null);
+    }
+
+    @Override
+    public StorageBox setAssociatedUmbrellaForBox(StorageBox box, Umbrella umbrella) {
+        box.setStoredUmbrella(umbrella);
+        box.setIsEmpty(false);
+        return storageBoxRepository.save(box);
+    }
+
+    @Override
+    public StorageBox markStorageAsEmpty(StorageBox box) {
+        box.setIsEmpty(true);
+        return storageBoxRepository.save(box);
     }
 }
